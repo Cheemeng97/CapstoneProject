1cb04fb6339a46e374e645b47f7631678eda1bd0
Oon Chee Meng <34565690+Cheemeng97@users.noreply.github.com>
2023-03-01 23:07:04 +0800

    Create networkScanner.py

---
+++ networkScanner.py
@@ -0,0 +1,52 @@
+from scapy.all import ARP, Ether, srp
+import socket
+import schedule
+import time
+from datetime import datetime
+import pandas as pd
+
+
+
+# def job():
+
+print("Start Scanning.....")
+
+#Create datafrome to store the data
+df = pd.DataFrame(columns=['Name', 'IP', 'MAC'])
+
+# Create ARP request packet
+packet = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="192.168.50.12/24")
+
+# Send packet and capture response
+results = srp(packet, timeout=3, verbose=0)[0]
+
+# Extract devices from response
+devices = []
+for result in results: # 0 = sent packet, 1 = received packet
+    devices.append({'ip': result[1].psrc, 'mac': result[1].hwsrc})
+
+currentDateTime = datetime.now()
+print("Devices connected to WiFi at"+ currentDateTime.strftime("%d/%m/%Y %H:%M:%S") + ":")
+# print("Name" + " "*18 + "IP" + " "*18 + "MAC")
+
+for device in devices:
+    try:
+        name = socket.gethostbyaddr(device['ip'])[0]
+    except socket.herror:
+        name = 'Unknown'
+
+    #store the data in dataframe
+    df = df.append({'Name': name, 'IP': device['ip'], 'MAC': device['mac']}, ignore_index=True)
+    # print(f"{name}    {device['ip']:16}    {device['mac']}")
+
+print(df)
+
+
+
+
+
+# schedule.every(40).seconds.do(job)
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
