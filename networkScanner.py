151c5afb224bdee071c24c9256e378776964c222
Oon Chee Meng <34565690+Cheemeng97@users.noreply.github.com>
2023-03-03 20:31:17 +0800

    Update networkScanner.py

--- networkScanner.py
+++ networkScanner.py
@@ -5,6 +5,8 @@ import time
 from datetime import datetime
 import pandas as pd
 import subprocess
+import warnings
+warnings.filterwarnings('ignore')
 
 def masterDevice():
     masterDeviceName = socket.gethostname()
@@ -12,26 +14,24 @@ def masterDevice():
     return masterDeviceName, masterDeviceIP
 
 # def job():
-
 print("Start Scanning.....")
 
-#Create datafrome to store the data
 df = pd.DataFrame(columns=['Master Device', 'Name', 'IP', 'MAC'])
+packet = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="192.168.1.0/24") # Create ARP request packet
 
-# Create ARP request packet
-packet = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="192.168.50.0/24")
+print("Scanning device.....")
 
-# Send packet and capture response
 results = srp(packet, timeout=3, verbose=0)[0]
 
-# Extract devices from response
+print("Generating results.....")
+
 devices = []
 for result in results: # 0 = sent packet, 1 = received packet
     devices.append({'ip': result[1].psrc, 'mac': result[1].hwsrc})
 
 currentDateTime = datetime.now()
-print("Devices connected to WiFi at"+ currentDateTime.strftime("%d/%m/%Y %H:%M:%S") + ":")
-# print("Name" + " "*18 + "IP" + " "*18 + "MAC")
+
+print("Devices connected to WiFi at "+ currentDateTime.strftime("%d/%m/%Y %H:%M:%S") + ":")
 
 for device in devices:
     try:
@@ -39,17 +39,25 @@ for device in devices:
     except socket.herror:
         name = 'Unknown'
 
-    #store the data in dataframe
     df = df.append({'Master Device':"", 'Name': name, 'IP': device['ip'], 'MAC': device['mac']}, ignore_index=True)
-    # print(f"{name}    {device['ip']:16}    {device['mac']}")
 
+#Check master device
 masterDeviceName, masterDeviceIP = masterDevice()
-
 df['Master Device'] = df['Name'].apply(lambda x: '*' if x == masterDeviceName else '')
 df['Master Device'] = df['IP'].apply(lambda x: '*' if x == masterDeviceIP else '')
 
 
 print(df)
+
+
+#Analyse the Total number of devices connected to WiFi
+totalDevices = len(df.index)
+print("Total Devices Connected to WiFi: " + str(totalDevices))
+knownDevices = len(df[df['Name'] != 'Unknown'].index)
+print("Known Devices Connected to WiFi: " + str(knownDevices))
+unknownDevices = len(df[df['Name'] == 'Unknown'].index)
+print("Unknown Devices Connected to WiFi: " + str(unknownDevices))
+
 
 
 
